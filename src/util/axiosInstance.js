87dc636ede2544a51f675a1b5ec65656802a1a94
zorojuro-03 <nandigama.keerthan@neogrowth.in>
2021-03-22 20:48:29 +0530

    changed backend url

--- src/util/axiosInstance.js
+++ src/util/axiosInstance.js
@@ -1,36 +1,36 @@
 // import { BACKEND_URL } from "#constants";
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://app.advancesuite.in:3061",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-  },
+	baseURL: "https://uat.advancesuite.in:3061",
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Content-Type": "application/json",
+	},
 });
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
-  function (config) {
-    // const { token } = localStorage;
-    // if (!!token) config.headers["x-auth-token"] = token;
-    // Do something before request is sent
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+	function (config) {
+		// const { token } = localStorage;
+		// if (!!token) config.headers["x-auth-token"] = token;
+		// Do something before request is sent
+		return config;
+	},
+	function (error) {
+		// Do something with request error
+		return Promise.reject(error);
+	}
 );
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response.data;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-  }
+	function (response) {
+		// Any status code that lie within the range of 2xx cause this function to trigger
+		// Do something with response data
+		return response.data;
+	},
+	function (error) {
+		// Any status codes that falls outside the range of 2xx cause this function to trigger
+		// Do something with response error
+		return Promise.reject(error);
+	}
 );
 export default axiosInstance;
