73fd56c2db64369678fef9650f76eee7b743d4a9
zorojuro-03 <nandigama.keerthan@neogrowth.in>
2021-05-10 15:57:31 +0530

    shwoing pdf updated

--- src/screens/catalog.js
+++ src/screens/catalog.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import { Redirect, useHistory, useParams } from "react-router-dom";
 import { partnerMapping } from "../store/partner_mapping";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfView = () => {
 	const { category, partner } = useParams();
@@ -24,17 +28,42 @@ const PdfView = () => {
 			}
 		}
 	}, []);
+	const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	function onDocumentLoadSuccess({ numPages }) {
+		setNumPages(numPages);
+	}
+
+	const ShowPages = () => {
+		let rows = [];
+		for (let i = 0; i < numPages; i++) {
+			rows.push(
+				<div>
+					<Page pageNumber={i + 1} />
+					<hr />
+				</div>
+			);
+		}
+		return rows;
+	};
+
 	return (
 		<div
 			style={{
-				height: "99vh",
-				overflowY: "scroll",
+				minHeight: "99vh",
 				display: "flex",
 				justifyContent: "center",
 				alignItems: "center",
 			}}
 		>
-			{data && <embed style={{ width: "100vw", height: "100vh" }} src={data} />}
+			{data && (
+				<div style={{ overflow: "scroll" }}>
+					<Document file={data} onLoadSuccess={onDocumentLoadSuccess}>
+						{numPages && <ShowPages />}
+					</Document>
+				</div>
+			)}
 		</div>
 	);
 };
