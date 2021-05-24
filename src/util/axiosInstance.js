698c9fe7bc231c201470e754d935479ef52c5472
zorojuro-03 <nandigama.keerthan@neogrowth.in>
2021-05-24 13:52:44 +0530

    changed url

--- src/util/axiosInstance.js
+++ src/util/axiosInstance.js
@@ -1,7 +1,7 @@
 // import { BACKEND_URL } from "#constants";
 import axios from "axios";
 const axiosInstance = axios.create({
-	baseURL: "http://0.0.0.0:3061",
+	baseURL: "https://uat.advancesuite.in:3061",
 	headers: {
 		"Access-Control-Allow-Origin": "*",
 		"Content-Type": "application/json",
