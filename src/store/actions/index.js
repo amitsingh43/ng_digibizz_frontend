bb64baa6d76e8cd52f5be60cea6545a3d602fe93
zorojuro-03 <nandigama.keerthan@neogrowth.in>
2021-06-07 18:39:47 +0530

    env changes

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -501,7 +501,7 @@ export const downloadReport = (downloadText, setDownloadText) => {
   const method = "GET";
   setDownloadText("Downloading...");
   const url =
-    "https://uat.advancesuite.in:3061/api/download_report?lead_id=" +
+    `${process.env.REACT_APP_BACKEND_URL}/api/download_report?lead_id=` +
     localStorage.getItem("lead_id");
   axios
     .request({
--- src/util/axiosInstance.js
+++ src/util/axiosInstance.js
@@ -1,7 +1,7 @@
 // import { BACKEND_URL } from "#constants";
 import axios from "axios";
 const axiosInstance = axios.create({
-	baseURL: "https://uat.advancesuite.in:3061",
+	baseURL: process.env.REACT_APP_BACKEND_URL,
 	headers: {
 		"Access-Control-Allow-Origin": "*",
 		"Content-Type": "application/json",
