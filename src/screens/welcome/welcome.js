acc948695292946ecd0d231b99d392fa5cab2c30
amitsingh42 <bpo-amit.singh@neogrowth.in>
2022-01-31 14:36:13 +0530

    top banner carousel added

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -9,6 +9,7 @@ import {
   Question,
   SuccessStories,
   MobileView,
+  TopCarousel,
 } from "./components";
 import lgBanner from "assets/Promotions/banner-lg.png";
 import smBanner from "assets/Promotions/banner-sm.png";
@@ -47,7 +48,8 @@ export default function Welcome() {
   return (
     <div style={{ fontFamily: "Rubik" }}>
       <div className="welcome">
-        <TopContent homepage_decrement={homepageDecrement} />
+        {/*<TopContent homepage_decrement={homepageDecrement} />*/}
+          <TopCarousel homepage_decrement={homepageDecrement}/>
       </div>
       <div className="explore-our-services">
         <ExploreOurServices />
